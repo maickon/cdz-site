e1a98244ba1834c890d52fcb528e51b473672814
Maickon Rangel <maickon4developers@gmail.com>
2026-03-15 10:37:22 -0300

    configurando pwa

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "meu-pwa-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/database.js",
+  "/icon.js",
+  "/style.css",
+  "/script.js",
+  "/pages/ARMORS.js",
+  "/pages/CHARACTER_CREATE.js",
+  "/pages/COMBAT.js",
+  "/pages/COSMO.js",
+  "/pages/HOME.js",
+  "/pages/MARKET.js",
+  "/pages/RESUME.js",
+  "/pages/SKILLS.js",
+  "/pages/TALENTS.js",
+  "/pages/TECHNIQUES.js",
+  "/img/icon-192.png",
+  "/img/icon-512.png",
+  "/athenas.jpg",
+  "/bg.jpg",
+  "/sparta.jpg",
+  "/m1.jpg",
+  "/m2.jpg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
